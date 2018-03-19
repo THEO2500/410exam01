16fbef338999162e9f31a33703409ef901592ba2
THEO2500 <worldpeace871219@gmail.com>
2018-03-19 20:23:00 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float n;
+        n=in.nextFloat();
+        System.out.printf("%.2f",n*1.6);
+
+    }
+}
